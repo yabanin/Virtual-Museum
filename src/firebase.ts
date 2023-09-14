bf679336c643d2c79bb9e9685628677f8bad2ccd
Yabanin <122198646+yabanin@users.noreply.github.com>
2023-09-15 00:51:04 +0900

    Add files via upload

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,17 +5,9 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyB0UFij6OPXgRGEGlijPhrDVwZhTPZ_HOw",
-  authDomain: "image-upload-bef40.firebaseapp.com",
-  databaseURL: "https://image-upload-bef40-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "image-upload-bef40",
-  storageBucket: "image-upload-bef40.appspot.com",
-  messagingSenderId: "643938080020",
-  appId: "1:643938080020:web:d8a8ab04f698116fbd5e01"
-};
-/*
-const firebaseApp = initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
   authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
@@ -23,8 +15,8 @@ const firebaseApp = initializeApp({
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-});
-*/
+};
+
 
 const app = initializeApp(firebaseConfig);
 
