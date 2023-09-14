ea7a68e6279d83e3ab8e71fe868be2cc847b53f5
Yabanin <122198646+yabanin@users.noreply.github.com>
2023-09-15 01:08:29 +0900

    Add files via upload

---
+++ src/App copy.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect } from "react";
+import styles from "./App.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { selectUser, login, logout } from "./features/userSlice";
+import { auth } from "./firebase";
+//Firebase ver9 compliant
+import { onAuthStateChanged } from "firebase/auth";
+import Feed from "./components/Feed";
+import Auth from "./components/Auth";
+
+const App: React.FC = () => {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    //Firebase ver9 compliant
+    const unSub = onAuthStateChanged(auth, (authUser) => {
+      if (authUser) {
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photoUrl: authUser.photoURL,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+    return () => {
+      unSub();
+    };
+  }, [dispatch]);
+
+  return (
+    <>
+      {user.uid ? (
+        <div className={styles.app}>
+          <Feed />
+        </div>
+      ) : (
+        <Auth />
+      )}
+    </>
+  );
+};
+
+export default App;
